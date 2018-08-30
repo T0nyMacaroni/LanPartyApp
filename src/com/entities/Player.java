f3e5d5d25d73cf5eed51d86a518366e897ea0058
AnthonyStulens <anthony.stulens@student.pxl.be>
2018-08-30 03:08:38 +0200

    Initial Commit

---
+++ src/com/entities/Player.java
@@ -0,0 +1,29 @@
+package com.entities;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+    private String name;
+    private String psnId;
+
+    public Player(String name, String psnId){
+        this.name = name;
+        this.psnId = psnId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPsnId(){
+        return psnId;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPsnId(String psnId){
+        this.psnId = psnId;
+    }
+}
