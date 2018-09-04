29cd5fead195bf4c69f2abf6bfa91b6d811088d2
Ben Teppers <ben.teppers@icloud.com>
2018-09-04 17:33:10 +0200

    IO Implementation [FIXED]

--- src/main/java/com/io/Reader.java
+++ src/main/java/com/io/Reader.java
@@ -1,5 +1,6 @@
 package com.io;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -13,22 +14,29 @@ import com.repositories.GamesRepository;
 import com.repositories.PlayerRepository;
 
 public class Reader {
-	
+
 	/**
 	 * Transfers file-data to object-data
-	 * @param fileName - String :: has to be an absolute path
+	 * 
+	 * @param fileName
+	 *            - String :: has to be an absolute path
 	 * @return Object
 	 */
 	private static Object readFile(String fileName) {
-		try (FileInputStream file = new FileInputStream(fileName);
-			 ObjectInputStream obj = new ObjectInputStream(file)) {
-			return obj.readObject();
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-			return null;
+		File file = new File(fileName);
+		if (file.exists()) {
+			try (FileInputStream in = new FileInputStream(fileName);
+					ObjectInputStream obj = new ObjectInputStream(in)) {
+				return obj.readObject();
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
+		return null;
+
 	}
-	
+
 	/**
 	 * Transfers all file-data from the default-dir to object-data
 	 */
@@ -36,15 +44,19 @@ public class Reader {
 		prepare_player_data(null);
 		prepare_games_data();
 	}
-	
+
 	/**
 	 * Convert player repository data from a file to an object.
-	 * @param path - Absolute path
+	 * 
+	 * @param path
+	 *            - Absolute path
 	 */
 	@SuppressWarnings("unchecked")
 	private static void prepare_player_data(String path) {
-		if (path == null) path = Settings.PLAYER_REPOSITORY_DATA;
+		if (path == null)
+			path = Settings.PLAYER_REPOSITORY_DATA;
 		List<Player> repo_player = (List<Player>) readFile(path);
+		if (repo_player == null) return;
 		List<Player> new_repo_player = new ArrayList<>();
 		repo_player.forEach(e -> {
 			e.build_player();
@@ -52,22 +64,26 @@ public class Reader {
 		});
 		PlayerRepository.createMap(new_repo_player);
 	}
-	
+
 	/**
 	 * Convert games repository data from a file to an object.
 	 */
 	@SuppressWarnings("unchecked")
 	private static void prepare_games_data() {
 		List<Game> repo_games = (List<Game>) readFile(Settings.GAMES_REPOSITORY_DATA);
+		if (repo_games == null) return;
 		GamesRepository.createMap(repo_games);
 	}
-	
+
 	/**
-	 * Imports player repository data from a file to an object using an absolute path
-	 * @param filePath - absolute path
+	 * Imports player repository data from a file to an object using an absolute
+	 * path
+	 * 
+	 * @param filePath
+	 *            - absolute path
 	 */
 	public static void import_player_repository(String filePath) {
 		prepare_player_data(filePath);
 	}
-	
+
 }
