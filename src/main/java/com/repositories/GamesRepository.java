3da4fa2ee9bd17075874928dcab5b77ebdfbc155
Ben Teppers <ben.teppers@icloud.com>
2018-09-04 02:46:54 +0200

    IRepository deprecated
    Statics are not allowed in Java 8 within an interface

--- src/main/java/com/repositories/GamesRepository.java
+++ src/main/java/com/repositories/GamesRepository.java
@@ -5,43 +5,36 @@ import java.util.List;
 
 import com.entities.Game;
 
-public class GamesRepository implements IRepository<Game> {
+public class GamesRepository {
 
-	public static List<Game> list = new ArrayList<>();
+	private static List<Game> list = new ArrayList<>();
 	
-	@Override
-	public void add(Game t) {
+	public static void add(Game t) {
 		list.add(t);
 	}
-
-	@Override
-	public void remove(int index) {
+	
+	public static void remove(int index) {
 		list.remove(index);
 	}
 
-	@Override
-	public void remove(Game t) {
+	public static void remove(Game t) {
 		list.remove(t);
 	}
 
-	@Override
-	public void get(int index) {
+	public static void get(int index) {
 		list.get(index);
 	}
 
-	@Override
-	public List<Game> getAll() {
+	public static List<Game> getAll() {
 		return list;
 	}
 
-	@Override
-	public void modify(Game o1, Game o2) {
+	public static void modify(Game o1, Game o2) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void modify(int index, Game o) {
+	public static void modify(int index, Game o) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/main/java/com/repositories/PlayerRepository.java
+++ src/main/java/com/repositories/PlayerRepository.java
@@ -5,37 +5,31 @@ import java.util.List;
 
 import com.entities.Player;
 
-public class PlayerRepository implements IRepository<Player> {
+public class PlayerRepository {
 
-	public static List<Player> list = new ArrayList<>();
+	private static List<Player> list = new ArrayList<>();
 	
-	@Override
-	public void add(Player t) {
+	public static void add(Player t) {
 		list.add(t);
 	}
 
-	@Override
-	public void remove(Player t) {
+	public static void remove(Player t) {
 		list.remove(t);
 	}
 	
-	@Override
-	public void remove(int index) {
+	public static void remove(int index) {
 		list.remove(index);
 	}
 
-	@Override
-	public void get(int index) {
+	public static void get(int index) {
 		list.get(index);
 	}
 
-	@Override
-	public List<Player> getAll() {
+	public static List<Player> getAll() {
 		return list;
 	}
 
-	@Override
-	public void modify(Player o1, Player o2) {
+	public static void modify(Player o1, Player o2) {
 		if (list.contains(o1)) {
 			int index = 0;
 			for (Player p : list) {
@@ -49,8 +43,7 @@ public class PlayerRepository implements IRepository<Player> {
 		}
 	}
 
-	@Override
-	public void modify(int index, Player o) {
+	public static void modify(int index, Player o) {
 		// TODO Auto-generated method stub
 		
 	}
