2562a24bad9df7a7154259e9ba60ba26d2d7e38b
AnthonyStulens <anthony.stulens@student.pxl.be>
2018-09-04 14:20:53 +0200

    Changing Player properties
    from String to StringProperty

--- src/main/java/com/entities/Player.java
+++ src/main/java/com/entities/Player.java
@@ -1,5 +1,8 @@
 package com.entities;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,42 +10,35 @@ import java.util.List;
 public class Player implements Serializable {
 	
 	private static final long serialVersionUID = 8658534901414231502L;
-	
-	private String name;
-	private String firstName;
-    private String psnId;
 
     private List<String> games;
 
     public Player(String name,String firstName, String psnId){
-        this.name = name;
-        this.firstName = firstName;
-        this.psnId = psnId;
+        this.name = new SimpleStringProperty(name);
+        this.firstName = new SimpleStringProperty(firstName);
+        this.psnId = new SimpleStringProperty(psnId);
         games = new ArrayList<>();
     }
 
-    public String getName(){
-        return name;
-    }
-
-    public String getFirstName() {
+    private StringProperty firstName;
+    public void setFirstName(String value) { firstNameProperty().set(value); }
+    public String getFirstName() { return firstNameProperty().get(); }
+    public StringProperty firstNameProperty() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    private StringProperty name;
+    public void setName(String value) { firstNameProperty().set(value); }
+    public String getName() { return firstNameProperty().get(); }
+    public StringProperty nameProperty() {
+        return name;
     }
 
-    public String getPsnId(){
+    private StringProperty psnId;
+    public void setPsnId(String value) { firstNameProperty().set(value); }
+    public String getPsnId() { return firstNameProperty().get(); }
+    public StringProperty PsnIdProperty() {
         return psnId;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setPsnId(String psnId){
-        this.psnId = psnId;
     }
 
 	public List<String> getGames() {
