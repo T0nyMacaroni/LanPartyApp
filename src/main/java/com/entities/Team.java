97dee802822338dae3e4030e72bc85888b58c500
Ben Teppers <ben.teppers@icloud.com>
2018-09-05 13:45:23 +0200

    Team rewritten using TDD technology

--- src/main/java/com/entities/Team.java
+++ src/main/java/com/entities/Team.java
@@ -1,7 +1,5 @@
 package com.entities;
 
-import com.entities.Player;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +8,39 @@ public class Team {
     private List<Player> players;
     private int teamSize;
 
+    public Team() {
+    	this(-1);
+    }
+    
     public Team(int teamSize){
-        this.setTeamName(teamName);
-        this.setTeamSize(teamSize);
+        this(null, teamSize);
+    }
+    
+    public Team (String teamName, int teamSize) {
+    	this.setTeamName(teamName);
+    	this.teamSize = teamSize;
         players = new ArrayList<>(teamSize);
     }
-
-    public void addPlayer(Player player){
+    
+    public void addPlayer(Player player) throws Exception {
+    	if (players.size() >= teamSize) {
+    		throw new Exception("Team size exceeded");
+    	}
         players.add(player);
+    }
+    
+    public void removePlayer(Player player) throws Exception {
+    	if (players.size() == 0) {
+    		throw new Exception("Team is already empty");
+    	}
+    	int index = 0;
+    	for (int i = 0; i < players.size(); i++) {
+    		if (players.get(i).getPsnId().equalsIgnoreCase(player.getPsnId())) {
+    			players.remove(index);
+    			return;
+    		}
+    	}
+    	throw new Exception("player with [" + player.getPsnId() + "] does not exist.");
     }
 
     public List<Player> getPlayers(){
@@ -36,7 +59,10 @@ public class Team {
 		return teamSize;
 	}
 
-	public void setTeamSize(int teamSize) {
+	public void setTeamSize(int teamSize) throws Exception {
+		if (players.size() > teamSize) {
+			throw new Exception("new team size is less than current player in the team.");
+		}
 		this.teamSize = teamSize;
 	}
 }
